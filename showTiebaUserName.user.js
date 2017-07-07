825f559101256ec82a9334588a1245f1629a0d99
8qwe24657913 <1197499769@qq.com>
2017-07-08 02:16:33 +0800

    设置改用prompt，简化代码

--- showTiebaUserName.user.js
+++ showTiebaUserName.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         贴吧显示真实ID
-// @version      0.2
+// @version      0.3
 // @namespace    https://github.com/8qwe24657913
 // @description  贴吧昵称掩盖了真实ID，认不出人了？这个脚本适合你
 // @author       8qwe24657913
@@ -21,57 +21,12 @@
         clip: rect(0px, auto, auto, auto);
     }
 }
-.frs-author-name:not(.shownUN), .p_author_name:not(.shownUN), .userinfo_username:not(.shownUN) {
+.frs-author-name:not(.shownUN), .p_author_name:not(.shownUN), .userinfo_username:not(.shownUN), #j_u_username .u_ddl_con_top > ul:not(.shownUN) {
     animation-duration: 0.001s;
     animation-name: showUserName;
 }
 .card_userinfo_title .userinfo_username, .card_userinfo_title .userinfo_username:hover {
-        max-width: max-content!important;
-}
-div#showUserNameSetting {
-    position: fixed;
-    margin: auto;
-    height: 20px;
-    width: 500px;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background: white;
-    box-shadow: 0 0 5px rgba(0, 0, 0, .5);
-    z-index: 1000;
-    display: flex;
-    padding: 10px;
-    align-items: center;
-}
-
-#showUserNameSetting input {
-    width: auto;
-    margin-right: 10px;
-    flex: 1;
-}
-
-#showUserNameSetting button {
-    -webkit-appearance: push-button;
-    border-width: 2px;
-    border-style: outset;
-    border-color: buttonface;
-    border-image: initial;
-    border-radius: unset;
-    background-color: buttonface;
-    background-image: none;
-}
-@keyframes showUserNameSetting {
-    from {
-        clip: rect(1px, auto, auto, auto);
-    }
-    to {
-        clip: rect(0px, auto, auto, auto);
-    }
-}
-#j_u_username .u_ddl_con_top > ul:not(.shownUN) {
-    animation-duration: 0.001s;
-    animation-name: showUserNameSetting;
+    max-width: max-content!important;
 }
 `;
     var style = document.createElement('style');
@@ -79,33 +34,24 @@ div#showUserNameSetting {
     document.documentElement.appendChild(style);
     // add setting
     var setting = localStorage.showUNSetting || '${un} (${nickname})';
-    document.addEventListener('animationstart', function listener(event) {
-        if (event.animationName !== 'showUserNameSetting') return;
-        document.removeEventListener('animationstart', listener, false);
-        var $set = $('<div id="showUserNameSetting"><input id="showUNInput" type="text"><button id="showUNButton">修改</button></div>').hide().appendTo('body');
-        var $input = $set.children('#showUNInput').val(setting);
-        $set.children('#showUNButton').on('click', function() {
-            var newSetting = $input.val();
-            if (newSetting !== setting) {
-                localStorage.showUNSetting = newSetting;
-                location.reload();
-            } else {
-                $set.hide();
-            }
-        });
-        $('<li class="u_showUN"><a href="javascript:">显ID设置</a></li>').on('click', function() {
-            $set.show();
-        }).appendTo($(event.target).addClass('shownUN'));
-    }, false);
+
+    function changeSetting() {
+        var newSetting = prompt('${un}表示真实ID，${nickname}表示昵称', setting);
+        if (newSetting && newSetting !== setting) {
+            localStorage.showUNSetting = newSetting;
+            location.reload();
+        }
+    }
     // main
-    document.addEventListener('animationstart', function(event) { // shouldn't use jquery, bacause it may not be loaded
+    document.addEventListener('animationstart', function(event) { // shouldn't use jQuery, bacause it may not be loaded
         if (event.animationName !== 'showUserName') return;
-        var target = event.target,
-            nickname = target.textContent,
-            node = target;
+        var target = event.target;
         target.classList.add('shownUN');
+        if (target.nodeName === 'UL') return $('<li class="u_showUN"><a href="javascript:">显ID设置</a></li>').on('click', changeSetting).appendTo(target); // jQuery is loaded here
+        var node = target;
         while (!node.hasAttribute('data-field')) node = node.parentElement;
-        var un = JSON.parse(node.getAttribute('data-field')).un;
+        var un = JSON.parse(node.getAttribute('data-field')).un,
+            nickname = target.textContent;
         if (nickname !== un) target.textContent = setting.replace(/\${un}/g, un).replace(/\${nickname}/g, nickname);
     }, false);
 })();
