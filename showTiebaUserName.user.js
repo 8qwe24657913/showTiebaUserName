b6d33e069a95a00a5d77381867ae87419c60f9b5
8qwe24657913 <1197499769@qq.com>
2017-07-09 02:34:37 +0800

    处理frs页用户名被切掉的情况，整理代码

--- showTiebaUserName.user.js
+++ showTiebaUserName.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         贴吧显示真实ID
-// @version      0.5
+// @version      0.6
 // @namespace    https://github.com/8qwe24657913
 // @description  贴吧昵称掩盖了真实ID，认不出人了？这个脚本适合你
 // @author       8qwe24657913
@@ -26,7 +26,7 @@
     animation-name: showUserName;
 }
 .card_userinfo_title .userinfo_username, .card_userinfo_title .userinfo_username:hover {
-    max-width: max-content!important;
+    max-width: 100%!important;
 }
 .userinfo_username + div[style]:not([class]):not([id]), img[src="//tb1.bdstatic.com/tb/cms/nickemoji/nickname_sign.png"] {
     display: none!important;
@@ -45,22 +45,30 @@
             location.reload();
         }
     }
+
+    function closestAttr(elem, attr) {
+        while (elem && !elem.hasAttribute(attr)) elem = elem.parentElement;
+        return elem ? elem.getAttribute(attr) : false;
+    }
     // main
     document.addEventListener('animationstart', function(event) { // shouldn't use jQuery, bacause it may not be loaded
         if (event.animationName !== 'showUserName') return;
         var target = event.target;
         target.classList.add('shownUN');
         if (target.nodeName === 'UL') return $('<li class="u_showUN"><a href="javascript:">显ID设置</a></li>').on('click', changeSetting).appendTo(target); // jQuery is loaded here
-        var node = target, un;
-        while (node && !node.hasAttribute('data-field')) node = node.parentElement;
-        if (node) {
-            un = JSON.parse(node.getAttribute('data-field')).un;
-        } else if (typeof PageData === 'object' && PageData.product === 'ihome')  { // ihome
+        var field = closestAttr(target, 'data-field'), un;
+        if (field) { // frs & pb & card
+            un = JSON.parse(field).un;
+        } else if (typeof PageData === 'object' && PageData.product === 'ihome') { // ihome
             un = target.nextElementSibling.getAttribute('data-username');
-        } else {
+        } else { // unknown
             return console.error('贴吧显示真实ID: 找不到真实ID', target);
         }
         var nickname = target.innerHTML.replace(/^<div[^>]*>(.*)<\/div>$/, '$1').replace(/<img src="\/\/tb1\.bdstatic\.com\/tb\/cms\/nickemoji\/nickname_sign\.png"[^>]*>/, '');
+        if (nickname.endsWith('...') && target.classList.contains('frs-author-name')) { // frs 用户名被切掉的情况
+            nickname = closestAttr(target.parentElement, 'title').split(' ')[1];
+            if (nickname === un) target.textContent = nickname; // 尽量显示完整
+        }
         if (nickname !== un) target.innerHTML = setting.replace(/\${un}/g, un).replace(/\${nickname}/g, nickname);
     }, false);
 })();
